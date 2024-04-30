fb7e80bf694f5214cef7bb5cc46f868669e8369d
Arden Feldt <feldt@ad.unc.edu>
2024-04-29 22:02:06 -0400

    Added Student Class

---
+++ Student.java
@@ -0,0 +1,47 @@
+public class Student {
+    private String id;
+    private Student oneFriend;
+    private Student twoFriend;
+    private Student threeFriend;
+
+    public Student(String id){
+        this.id = id;
+        this.oneFriend = null;
+        this.twoFriend = null;
+        this.threeFriend = null;
+    }
+    public Student(String id, Student oneFriend, Student twoStudent, Student threeStudent){
+        this.id = id;
+        this.oneFriend = oneFriend;
+        this.twoFriend = twoStudent;
+        this.threeFriend = threeStudent;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Student getOneFriend() {
+        return oneFriend;
+    }
+
+    public Student getTwoFriend() {
+        return twoFriend;
+    }
+
+    public Student getThreeFriend() {
+        return threeFriend;
+    }
+
+    public void setOneFriend(Student oneFriend) {
+        this.oneFriend = oneFriend;
+    }
+
+    public void setTwoFriend(Student twoFriend) {
+        this.twoFriend = twoFriend;
+    }
+
+    public void setThreeFriend(Student threeFriend) {
+        this.threeFriend = threeFriend;
+    }
+}
