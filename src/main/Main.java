8b2b448c642dd6ff60bf271a7ef847f0c68624f7
Arden Feldt <feldt@ad.unc.edu>
2024-05-01 16:21:43 -0400

    Java naming conventions

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,7 @@
 package src.main;
 
-import src.main.googleFormReader.PathLocator;
-
-public class main {
+public class Main {
     public static void main(String[] args) {
-
         Grade grade = new Grade();
 
         // Checking it all works
