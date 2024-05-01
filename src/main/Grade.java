6d450afd489ffccf3d273efe4f7c08a9a827396c
Arden Feldt <feldt@ad.unc.edu>
2024-05-01 16:20:43 -0400

    Cleaned up main

--- src/main/Grade.java
+++ src/main/Grade.java
@@ -1,11 +1,20 @@
 package src.main;
 
 import java.util.HashSet;
+
+import src.main.googleFormReader.PathLocator;
 import src.main.googleFormReader.ReadStudentResponses;
 
 public class Grade {
     HashSet<Student> students;
-    public Grade(String path){
+    public Grade(){
+        PathLocator pathLocator = new PathLocator();
+
+        // Remove this after UI implementation
+        pathLocator.setPath("C:\\Users\\ethan\\IdeaProjects\\Seating-Chart\\src\\main\\googleFormReader\\studentForm");
+
+        String path = pathLocator.getPath();
+
         ReadStudentResponses readStudentResponses = new ReadStudentResponses(path);
         readStudentResponses.knitStudents();
         students = new HashSet<>(readStudentResponses.getStudents());
--- src/main/main.java
+++ src/main/main.java
@@ -4,13 +4,8 @@ import src.main.googleFormReader.PathLocator;
 
 public class main {
     public static void main(String[] args) {
-        // Implement UI that gets studentForm.csv path
-        PathLocator pathLocator = new PathLocator();
 
-        // Remove this after UI implementation
-        pathLocator.setPath("C:\\Users\\ethan\\IdeaProjects\\Seating-Chart\\src\\main\\googleFormReader\\studentForm");
-
-        Grade grade = new Grade(pathLocator.getPath());
+        Grade grade = new Grade();
 
         // Checking it all works
         System.out.println(grade.getStudents());
