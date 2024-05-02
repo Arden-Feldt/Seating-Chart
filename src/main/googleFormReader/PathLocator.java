007d0ef5c1185927f43cfc06388ee3823a14ad63
Arden Feldt <feldt@ad.unc.edu>
2024-05-02 13:37:28 -0400

    path logic

--- src/main/googleFormReader/PathLocator.java
+++ src/main/googleFormReader/PathLocator.java
@@ -1,5 +1,7 @@
 package src.main.googleFormReader;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class PathLocator {
@@ -9,6 +11,27 @@ public class PathLocator {
     Scanner scanner = new Scanner(System.in);
     System.out.println("What is the path to your file?");
     path = scanner.next();
+
+    boolean foundPath = false;
+
+      while (!foundPath) {
+          try {
+              File file = new File(path);
+
+              if (file.exists() && !file.isDirectory() && path.toLowerCase().endsWith(".csv")) {
+                  foundPath = true;
+                  System.out.println("Valid CSV file path: " + path);
+              } else {
+                  System.out.println("Invalid CSV file path. Please enter a valid path to a CSV file.");
+                  path = scanner.next();
+              }
+          } catch (Exception e) {
+              System.out.println("Not a valid path; error: " + e.getMessage());
+          }
+      }
+
+      scanner.close();
+
   }
 
   public String getPath() {
