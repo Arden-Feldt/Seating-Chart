3368097f43f43f8176b16d284dd7aab21293dca8
Arden Feldt <feldt@ad.unc.edu>
2024-05-14 14:39:58 -0400

    added reader (does nothing rn)

---
+++ src/main/miniZinc/DznReader.java
@@ -0,0 +1,25 @@
+package src.main.miniZinc;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class DznReader {
+    public DznReader(){
+        // Will read in the solution and make it pretty, currently just copies what DznWriter put down
+
+        String filePath = "src/main/miniZinc/seatingdata.dzn";
+
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(filePath));
+            for (String line : lines) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
