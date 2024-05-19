d1e6c90400bc9b4166b909c0fbb579ffb651b0b1
Arden Feldt <feldt@ad.unc.edu>
2024-05-19 18:54:18 -0400

    skeleton for reading in teacher input

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,15 +4,23 @@ import src.main.miniZinc.DznWriter;
 import src.main.miniZinc.MiniZincExecutor;
 import src.main.miniZinc.SolutionParser;
 import src.main.miniZinc.StudentTransformer;
+import src.main.teacherInput.TeacherInputReader;
 
 public class Main {
 
-  public static int TABLENUMBER = 100; // Max number of tables available;
+  public static int TABLENUMBER; // Max number of tables available;
   public static final int FRIENDNUMBER = 3; // DO NOT FUCK WITH - How many friends each student has
-  public static int SEATNUMBER = 8; // Number of seats at a table;
+  public static int SEATNUMBER; // Number of seats at a table;
   public static int TIMESTOP = 1000; // Time stop is in milliseconds;
+  // ^ init with function that ramps with students and constraints
 
   public static void main(String[] args) {
+    // Init teacher stuff - maybe put this somewhere else cleaner
+    TeacherInputReader teacherInputReader = new TeacherInputReader();
+    TABLENUMBER = teacherInputReader.getTableNumber();
+    SEATNUMBER = teacherInputReader.getSeatNumber();
+
+    // Make the grade
     Grade grade = new Grade();
 
     // Checking it all works
---
+++ src/main/teacherInput/TeacherInputReader.java
@@ -0,0 +1,26 @@
+package src.main.teacherInput;
+
+public class TeacherInputReader {
+    // TODO: Implement this
+    // reads who shouldn't get put together
+    // returns that info in the format of a mzn
+    // is somehow injected into mzm (called by MznWriter?)
+
+    private int numConstraints;
+
+    public TeacherInputReader(){
+
+    }
+
+    public int getTableNumber(){
+        return 100;
+    }
+
+    public int getSeatNumber() {
+        return 8;
+    }
+
+    public int getNumberConstraints(){
+        return numConstraints;
+    }
+}
